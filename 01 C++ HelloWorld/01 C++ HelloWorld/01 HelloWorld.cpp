11e142f3d5530c70dfe8d255e6549ccbb5650471
Yakun Wang <30728487+yakunlp@users.noreply.github.com>
2023-08-06 19:50:25 +0800

    first commit

---
+++ 01 C++ HelloWorld/01 C++ HelloWorld/01 HelloWorld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//1.单行注释
+
+//.多行注释
+
+/*
+	main是一个程序的入口
+
+*/
+
+
+int main()
+{
+	cout << "hello C++ world" << endl; //to show hello C++ world in the screen.
+	system("pause");
+
+	return 0;
+}
